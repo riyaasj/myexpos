98445e4ceda18dff9a01723c48fdc0ec022a686f
riya <riyasebyjohn@gmail.com>
2025-03-16 23:11:41 +0530

    ellam

---
+++ spl/batch17.sh
@@ -0,0 +1,10 @@
+load --idle $HOME/myexpos/expl/expl_progs/idle_1.expl
+load --init $HOME/myexpos/expl/samples/odd.expl
+load --os $HOME/myexpos/spl/spl_progs/os_startup_13.spl
+load --int=7 $HOME/myexpos/spl/spl_progs/int7_15.spl
+load --int=10 $HOME/myexpos/spl/spl_progs/int10_14.spl
+load --int=timer $HOME/myexpos/spl/spl_progs/sample_timer_14.spl
+load --module 7 $HOME/myexpos/spl/spl_progs/mod_7_15.spl
+load --module 5 $HOME/myexpos/spl/spl_progs/mod_5_14.spl
+load --module 4 $HOME/myexpos/spl/spl_progs/mod_4_15.spl
+load --module 0 $HOME/myexpos/spl/spl_progs/mod_0_15.spl
